d2e88f89f270518e1721172c2aa6e1e6e2d69dbb
FRLCR <franco.frlcr@gmail.com>
2023-01-18 11:49:43 -0300

    BIG FIX

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 mongoose.set("strictQuery", true);
 mongoose.connect("mongodb+srv://francotds:franco123@dbf.rc79oqk.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser : true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true, // SI
 })
 .then(db => console.log("CONECTADO"))
 .catch(error => console.log(error))
