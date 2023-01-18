3e922c5214f9b1e09ac3611f1034fdf4a05465a5
FRLCR <franco.frlcr@gmail.com>
2023-01-18 20:14:16 -0300

    Redirect Server 6

--- src/routes/producto.route.js
+++ src/routes/producto.route.js
@@ -5,7 +5,7 @@ import {verifyToken, onlyMods} from '../middlewares/index.js'
 const router = Router()
 
 //  [verifyToken, onlyMods],
-router.get("/", [verifyToken, onlyMods], producto.getProductList)
+router.get("/", [onlyMods, verifyToken], producto.getProductList)
 router.post('/', producto.newProduct)
 router.delete('/:productId', producto.deleteProduct)
 router.put('/:productId', producto.updateProduct)
