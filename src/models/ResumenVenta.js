8ccfee70910267895c59a051897b530e0a45e754
FRLCR <franco.frlcr@gmail.com>
2023-01-20 14:26:59 -0300

    Resumen VEntas - Optimizado

--- src/models/ResumenVenta.js
+++ src/models/ResumenVenta.js
@@ -23,10 +23,4 @@ export function ResumenVenta(){
     let cantidadesCompradas = []
     let totalRecaudado
 }
-
-export function addProduct(producto, cantidad){
-        this.productosComprados.push(producto)
-        this.cantidadesCompradas.push(cantidad)
-    }
-
 export default class{}
--- src/routes/producto.route.js
+++ src/routes/producto.route.js
@@ -7,6 +7,8 @@ const router = Router()
 //  [verifyToken, onlyMods],
 router.get("/", [verifyToken, onlyMods], producto.getProductList)
 router.post('/', producto.newProduct)
+
+
 router.delete('/:productId', producto.deleteProduct)
 router.put('/:productId', producto.updateProduct)
 router.get('/:productId', producto.getProduct)
