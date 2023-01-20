c451ac8529e7ba354caef1625757711eb57926b1
FRLCR <franco.frlcr@gmail.com>
2023-01-20 16:23:13 -0300

    Resumen Ventas Optimizado v2

--- src/models/ResumenVenta.js
+++ src/models/ResumenVenta.js
@@ -1,26 +1,8 @@
-/* import {Schema, model} from 'mongoose'
-
-const resumenVentaSchema = new Schema({
-    comprador: String,
-    productosComprados: [{type: String}]  ,
-    cantidadesCompradas: [{type: Number}],
-    totalRecaudado: Number,
-}, {
-    timestamps: false,
-    versionKey: false
-})
-
-resumenVentaSchema.function.addProductList = (nombreProducto, cantidadComprada) =>{
-    this.productosComprados.push(nombreProducto)
-    this.cantidadesCompradas.push(cantidadComprada)
-}
-export default model('Resumen de Venta', resumenVentaSchema) */
-
-
 export function ResumenVenta(){
     let comprador;
     let productosComprados = []
     let cantidadesCompradas = []
     let totalRecaudado
 }
+
 export default class{}
