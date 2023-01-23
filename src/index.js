a6141a2a9051952bfaacb23c214428849605d7d4
FRLCR <franco.frlcr@gmail.com>
2023-01-23 14:33:21 -0300

    Historial Ventas Fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import app from './app.js'
 import './database.js' 
 
-/* const serverPort = 3000 */
-const serverPort = 80
+const serverPort = 3000
 
 app.listen(serverPort)
 console.log("Server online en el puerto ", serverPort )
