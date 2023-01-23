f06ef6f8a6188e692c76d4c7506cc80db41e08b7
FRLCR <franco.frlcr@gmail.com>
2023-01-23 16:10:12 -0300

    Totales Recaudados, manejo de Stock

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import app from './app.js'
 import './database.js' 
 
-const serverPort = 80
+const serverPort = 3000
 
 app.listen(serverPort)
 console.log("Server online en el puerto ", serverPort )
